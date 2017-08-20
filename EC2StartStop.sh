848ff325eced33e501f321d52b8c1e37b19bee0f
thierryturpin <thierry@turpin.be>
2017-08-20 08:25:10 +0200

    Configuration file via first parameter

--- EC2StartStop.sh
+++ EC2StartStop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /Users/thierryturpin/2.7EC2StartStop/bin/activate
 cd /Users/thierryturpin/Documents/PycharmProjects/EC2StartStop/EC2StartStop/
-python EC2StartStop.py
+python EC2StartStop.py /Users/thierryturpin/Documents/PycharmProjects/EC2StartStop/conf.json
