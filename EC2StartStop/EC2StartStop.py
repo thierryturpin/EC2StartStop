c4bbc136ec0b1b02b562311bb6523c3f1081f790
thierryturpin <thierry@turpin.be>
2018-04-15 10:36:47 +0200

    Add windows option to use putty or WSL

--- EC2StartStop/EC2StartStop.py
+++ EC2StartStop/EC2StartStop.py
@@ -14,37 +14,43 @@ import simplejson
 import os
 import sys
 
-#TODO
+
+# TODO
 # When connecting and instance is down prompt to start
 
 def get_time():
-    return(time.strftime("%d/%m/%Y %H:%M:%S", time.localtime()))
+    return (time.strftime("%d/%m/%Y %H:%M:%S", time.localtime()))
+
 
 def get_ec2_monitor(instances):
     """Format dataframe, column order. Only dipslay instaces of the last month. Order by instance description.
     """
     global filters
     ec2_monitor = pd.DataFrame(get_instance_attributes(instances))
-    ec2_monitor = ec2_monitor[['Name', 'PrivateIpAddress', 'PublicIp', 'State', 'LaunchTime', 'InstanceId', 'FQDN', 'uptime_hours', 'StateCode', 'Platform', 'osuser', 'pemfile']]
+    ec2_monitor = ec2_monitor[
+        ['Name', 'PrivateIpAddress', 'PublicIp', 'State', 'LaunchTime', 'InstanceId', 'FQDN', 'uptime_hours',
+         'StateCode', 'Platform', 'osuser', 'pemfile']]
     if filters == True:
         date_from = datetime.today() - relativedelta(months=1)
         ec2_monitor = ec2_monitor[ec2_monitor['LaunchTime'] > date_from]
-    ec2_monitor.sort_values(['Name',],ascending=[True],inplace=True)
+    ec2_monitor.sort_values(['Name', ], ascending=[True], inplace=True)
     ec2_monitor = ec2_monitor.reset_index(drop=True)
     return ec2_monitor
+
 
 def dns_records_clean(ln):
     ln['IP'] = ln['ResourceRecords'][0]['Value']
     ln['FQDN'] = ln['Name'][:-1]
     return ln
 
+
 def get_fqdns():
     """If a hosted zone is foreseen, get DNS records.
     """
     if get_config('HostedZoneId') != None:
         client = boto3.client('route53', aws_access_key_id=get_config('aws_access_key_id'), \
-                          aws_secret_access_key=get_config('aws_secret_access_key'), \
-                          region_name=get_config('region'))
+                              aws_secret_access_key=get_config('aws_secret_access_key'), \
+                              region_name=get_config('region'))
 
         response = client.list_resource_record_sets(HostedZoneId=get_config('HostedZoneId'))
         dns_records = pd.DataFrame(response['ResourceRecordSets'])
@@ -85,7 +91,7 @@ def get_instance_attributes(linstances):
                 else:
                     attributes['osuser'] = 'ec2-user'
                     attributes['pemfile'] = default_pemfile
-                    for y in range(0,len(instance_connect)):
+                    for y in range(0, len(instance_connect)):
                         if attributes['InstanceId'] in instance_connect[y]['InstanceId']:
                             attributes['osuser'] = instance_connect[y]['osuser']
                             attributes['pemfile'] = instance_connect[y]['pemfile']
@@ -103,20 +109,21 @@ def get_instance_attributes(linstances):
                     attributes['uptime_hours'] = ''
                 attributes['PrivateIpAddress'] = linstance['Instances'][x]['PrivateIpAddress']
                 if 'Association' in linstance['Instances'][x]['NetworkInterfaces'][0]:
-                    attributes['PublicIp'] = linstance['Instances'][x]['NetworkInterfaces'][0]['Association']['PublicIp']
+                    attributes['PublicIp'] = linstance['Instances'][x]['NetworkInterfaces'][0]['Association'][
+                        'PublicIp']
                 else:
                     attributes['PublicIp'] = '-'
                 if HosteZoneLookup:
                     num_dns_records = len(dns_records[(dns_records['IP'] == attributes['PrivateIpAddress'])]['FQDN'])
                     if num_dns_records == 1:
-                        attributes['FQDN'] = dns_records[(dns_records['IP'] == attributes['PrivateIpAddress'])]['FQDN'].values[0]
+                        attributes['FQDN'] = \
+                        dns_records[(dns_records['IP'] == attributes['PrivateIpAddress'])]['FQDN'].values[0]
                     else:
                         attributes['FQDN'] = '-'
                 else:
                     attributes['FQDN'] = '-'
                 dattributes.append(attributes)
     return dattributes
-
 
 
 def get_config(attr):
@@ -127,11 +134,13 @@ def get_config(attr):
         attr = configdata[attr]
         return attr
 
+
 def get_client():
     client = boto3.client('ec2', aws_access_key_id=get_config('aws_access_key_id'), \
                           aws_secret_access_key=get_config('aws_secret_access_key'), \
                           region_name=get_config('region'))
     return client
+
 
 def get_instances():
     try:
@@ -140,6 +149,7 @@ def get_instances():
         error_text = 'Unexpected error: {0}'.format(e)
         click.echo(click.style(error_text, fg='red'))
     return instances
+
 
 def get_instances_state():
     """Get data frame with all instances and return the refresh rate for the CLI.
@@ -156,22 +166,23 @@ def get_instances_state():
         print '{} - Press: CTRL-C for all interactions'.format(get_time())
         for row in ec2_df.itertuples():
             table_line = '{0:3}|{1:{nameLen}}|{7:{fqdnLen}}|{2:15}|{3:15}|{4:14}|{5}|{8:4}|{6:19}|' \
-            .format(row[0], row[1], row[2], row[3], row[4], row[5].strftime("%d/%m %H:%M"), row[6], row[7], row[8], \
-            nameLen=nameLen, fqdnLen=fqdnLen)
+                .format(row[0], row[1], row[2], row[3], row[4], row[5].strftime("%d/%m %H:%M"), row[6], row[7], row[8], \
+                        nameLen=nameLen, fqdnLen=fqdnLen)
             if row[4] == 'running':
                 if row[8] > 8:
                     bold = True
                 else:
                     bold = False
                 click.echo(click.style(table_line, fg='green', bold=bold))
-            elif row[4] in ('pending','stopping'):
+            elif row[4] in ('pending', 'stopping'):
                 click.echo(click.style(table_line, fg='yellow'))
                 refresh_rate = 3
             else:
-                click.echo(click.style(table_line, fg='white',))
+                click.echo(click.style(table_line, fg='white', ))
     except:
         pass
     return refresh_rate
+
 
 @click.command()
 @click.argument('conf_file')
@@ -188,12 +199,14 @@ def get_config_file(conf_file):
         click.echo(click.style('Parameter is not a file, configuration file. See --help.', fg='red'))
         time.sleep(3)
 
+
 def main():
     try:
         while True:
             time.sleep(get_instances_state())
     except(KeyboardInterrupt):
         handle_main()
+
 
 def handle_main():
     global filters
@@ -221,10 +234,11 @@ def handle_main():
         time.sleep(2)
         main()
 
+
 @click.command()
 @click.option('--start', prompt='Instance to start', type=click.INT, help='Select stopped instance')
-@click.argument('conf_file') #Do not remove, enforced by click
-def handle_start(start,conf_file):
+@click.argument('conf_file')  # Do not remove, enforced by click
+def handle_start(start, conf_file):
     instances = get_instances()
     ec2_monitor = get_ec2_monitor(instances)
     try:
@@ -237,16 +251,17 @@ def handle_start(start,conf_file):
             response = get_client().start_instances(InstanceIds=instance_ids)
             click.echo('Response %s' % response)
         else:
-            click.echo(click.style('Instance is not in state stopped',fg='magenta'))
+            click.echo(click.style('Instance is not in state stopped', fg='magenta'))
     except:
-        click.echo(click.style('Invalid line index selection start',fg='magenta'))
+        click.echo(click.style('Invalid line index selection start', fg='magenta'))
     time.sleep(2)
     main()
+
 
 @click.command()
 @click.option('--stop', prompt='Instance to stop', type=click.INT, help='Select started instance')
 @click.argument('conf_file')
-def handle_stop(stop,conf_file): #Do not remove, enforced by click
+def handle_stop(stop, conf_file):  # Do not remove, enforced by click
     instances = get_instances()
     ec2_monitor = get_ec2_monitor(instances)
     try:
@@ -259,11 +274,12 @@ def handle_stop(stop,conf_file): #Do not remove, enforced by click
             response = get_client().stop_instances(InstanceIds=instance_ids)
             click.echo('Response %s' % response)
         else:
-            click.echo(click.style('Instance is not in state running',fg='magenta'))
+            click.echo(click.style('Instance is not in state running', fg='magenta'))
     except:
-        click.echo(click.style('Invalid line index selection stop',fg='magenta'))
+        click.echo(click.style('Invalid line index selection stop', fg='magenta'))
     time.sleep(2)
     main()
+
 
 def handle_stopall():
     instances = get_instances()
@@ -277,21 +293,23 @@ def handle_stopall():
     time.sleep(2)
     main()
 
+
 def handle_exit():
     click.clear()
     cover()
     sys.exit()
 
+
 @click.command()
 @click.option('--connect', prompt='Instance to connect', type=click.INT, help='Select started instance')
 @click.argument('conf_file')
-def handle_connect(connect,conf_file): #Do not remove, enforced by click
+def handle_connect(connect, conf_file):  # Do not remove, enforced by click
     instances = get_instances()
     ec2_monitor = get_ec2_monitor(instances)
     MACOS = sys.platform.startswith('darwin')
     WINDOWS = sys.platform.startswith('win')
     domain = get_config('domain')
-    domainUser= get_config('domainUser')
+    domainUser = get_config('domainUser')
 
     state = ec2_monitor['State'][int(connect)]
     pemfile = ec2_monitor['pemfile'][int(connect)]
@@ -301,10 +319,14 @@ def handle_connect(connect,conf_file): #Do not remove, enforced by click
 
     if WINDOWS:
         win = 'start mstsc /v {}'.format(addr)
-        lin = 'start cmd.exe /K bash ~ -c "ssh {}@{} -i {}"'.format(ec2_monitor['osuser'][connect], addr, pemfile)
+        if get_config('winssh') == 'WSL':
+            lin = 'start cmd.exe /K bash ~ -c "ssh {}@{} -i {}"'.format(ec2_monitor['osuser'][connect], addr, pemfile)
+        elif get_config('winssh') == 'putty':
+            lin = 'start putty.exe -ssh -i {} {}@{}'.format(pemfile, ec2_monitor['osuser'][connect], addr)
     elif MACOS:
         win = 'open rdp://full%20address=s:{}:3389&domain=s:{}&username=s:{}'.format(addr, domain, domainUser)
-        lin = 'osascript -e \'tell application "Terminal" to do script "ssh {}@{} -i {}"\''.format(ec2_monitor['osuser'][connect], addr, pemfile)
+        lin = 'osascript -e \'tell application "Terminal" to do script "ssh {}@{} -i {}"\''.format(
+            ec2_monitor['osuser'][connect], addr, pemfile)
 
     try:
         if state == 'running':
@@ -316,9 +338,9 @@ def handle_connect(connect,conf_file): #Do not remove, enforced by click
             click.echo(click.style(cmd, fg='yellow'))
 
         else:
-            click.echo(click.style('Instance is not in state running',fg='magenta'))
+            click.echo(click.style('Instance is not in state running', fg='magenta'))
     except:
-        click.echo(click.style('Invalid line index selection connect',fg='magenta'))
+        click.echo(click.style('Invalid line index selection connect', fg='magenta'))
     time.sleep(3)
     main()
 
@@ -344,8 +366,9 @@ def cover():
     T. Turpin
                                                                           
                                                                           '''
-    click.echo(click.style(covertext,fg='green'))
+    click.echo(click.style(covertext, fg='green'))
     time.sleep(2)
+
 
 if __name__ == '__main__':
     global filters
